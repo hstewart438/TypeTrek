7599e43e9a9502214cba14e71b28cbcc003bbf82
oabrams613 <oabrams@ufl.edu>
2025-12-01 23:31:17 -0500

    Added dark mode toggle across a number of pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,17 @@ import { AuthContextProvider } from './context/AuthContext';
 import { router } from './router';
 
 import './styles/index.css';
+import ThemeToggle from "./components/ThemeToggle"; 
 
 //render app
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <div className="app-root">
         <AuthContextProvider>
             <RouterProvider router={router}/>
+            <ThemeToggle /> 
         </AuthContextProvider>
+        </div>
     </React.StrictMode>
 );
